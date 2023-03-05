9301f78249d1742e0ea89bbd20e637ed1cd06f4f
AdkaJonasova <xjonaso1@fi.muni.cz>
2023-03-05 16:41:51 +0100

    Added basic convert hex to bytes
    Signed-off-by: AdkaJonasova <xjonaso1@fi.muni.cz>

--- src/Converter.java
+++ src/Converter.java
@@ -1,2 +1,15 @@
 public class Converter {
+
+    public String HexToBytes(String hexStr) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < hexStr.length() - 1; i+=2) {
+            var hexPart = hexStr.substring(i, i + 2);
+            var intFromHex = Integer.parseInt(hexPart, 16);
+            builder.append((char) intFromHex);
+        }
+
+        return builder.toString();
+    }
+
 }
