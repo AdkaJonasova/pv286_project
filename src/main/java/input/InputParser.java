37ee94c99c892724786ca0843995780b30ab68a2
AdkaJonasova <xjonaso1@fi.muni.cz>
2023-03-12 21:53:31 +0100

    Added InputParser class
    Signed-off-by: AdkaJonasova <xjonaso1@fi.muni.cz>

---
+++ src/main/java/input/InputParser.java
@@ -0,0 +1,18 @@
+package input;
+
+public class InputParser {
+    private boolean fromFlag = false;
+    private boolean toFlag = false;
+    private boolean fromOptionsFlag = false;
+    private boolean toOptionsFlag = false;
+    private boolean inputFileFlag = false;
+    private boolean outputFileFlag = false;
+    private boolean delimiterFlag = false;
+    private boolean helpFlag = false;
+
+    private String fromRepresentation = "";
+    private String toRepresentation = "";
+    private String fromOptions = "";
+    private String toOptions = "";
+    private String delimiter = "";
+}
