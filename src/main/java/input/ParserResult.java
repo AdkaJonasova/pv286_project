1cd5d2c14d8165ec8cf526e8910c37ced7cd760a
AdkaJonasova <xjonaso1@fi.muni.cz>
2023-03-12 22:39:23 +0100

    Added ParserResult class
    Signed-off-by: AdkaJonasova <xjonaso1@fi.muni.cz>

---
+++ src/main/java/input/ParserResult.java
@@ -0,0 +1,44 @@
+package input;
+
+public class ParserResult {
+
+    private String from;
+    private String to;
+    private String fromOption;
+    private String toOption;
+
+    //region Setters and Getters
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getFromOption() {
+        return fromOption;
+    }
+
+    public void setFromOption(String fromOption) {
+        this.fromOption = fromOption;
+    }
+
+    public String getToOption() {
+        return toOption;
+    }
+
+    public void setToOption(String toOption) {
+        this.toOption = toOption;
+    }
+
+    //endregion
+}
