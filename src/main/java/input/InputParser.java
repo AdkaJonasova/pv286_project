5667d0da36a0941579bf3263727347b799bc54e0
AdkaJonasova <xjonaso1@fi.muni.cz>
2023-03-12 21:59:35 +0100

    Added parse flag function
    Signed-off-by: AdkaJonasova <xjonaso1@fi.muni.cz>

--- src/main/java/input/InputParser.java
+++ src/main/java/input/InputParser.java
@@ -1,6 +1,7 @@
 package input;
 
 public class InputParser {
+
     private boolean fromFlag = false;
     private boolean toFlag = false;
     private boolean fromOptionsFlag = false;
@@ -15,4 +16,24 @@ public class InputParser {
     private String fromOptions = "";
     private String toOptions = "";
     private String delimiter = "";
+
+    private void parseFlag(String argument) {
+        if (argument.equals("-f")) {
+            fromFlag = true;
+        } else if (argument.equals("-t")) {
+            toFlag = true;
+        } else if (argument.equals("-i")) {
+            inputFileFlag = true;
+        } else if (argument.equals("-o")) {
+            outputFileFlag = true;
+        } else if (argument.equals("-h")) {
+            helpFlag = true;
+        } else if (argument.startsWith("--from-options")) {
+            fromOptionsFlag = true;
+        } else if (argument.startsWith("--to-options")) {
+            toOptionsFlag = true;
+        } else {
+            // chyba
+        }
+    }
 }
