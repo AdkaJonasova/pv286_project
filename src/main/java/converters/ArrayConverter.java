67c87992ea231596d805a49368ada761f53ebe0b
Michal Badin <badin.miso@gmail.com>
2023-03-14 19:24:46 +0100

    Added ArrayConverter with blank methods
    Signed-off-by: Michal Badin <badin.miso@gmail.com>

---
+++ src/main/java/converters/ArrayConverter.java
@@ -0,0 +1,14 @@
+package converters;
+
+
+public class ArrayConverter extends Converter {
+	@Override
+	public String convertTo(String bitStr, String option) {
+		return null;
+	}
+
+	@Override
+	public String convertFrom(String str, String option) {
+		return null;
+	}
+}
