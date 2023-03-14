2a3178bd1d2280b0ff30aed07b59c9cb4ff1573e
AdkaJonasova <xjonaso1@fi.muni.cz>
2023-03-14 09:16:14 +0100

    Changed ParserResult fields to final
    Signed-off-by: AdkaJonasova <xjonaso1@fi.muni.cz>

--- src/main/java/input/ParserResult.java
+++ src/main/java/input/ParserResult.java
@@ -2,14 +2,14 @@ package input;
 
 public class ParserResult {
 
-    private String from;
-    private String to;
-    private String fromOption;
-    private String toOption;
-    private String inputFile;
-    private String outputFile;
-    private String delimiter;
-    private boolean shouldPrintHelp;
+    private final String from;
+    private final String to;
+    private final String fromOption;
+    private final String toOption;
+    private final String inputFile;
+    private final String outputFile;
+    private final String delimiter;
+    private final boolean shouldPrintHelp;
 
     public ParserResult(String from, String to, String fromOption, String toOption, String inputFile, String outputFile,
                         String delimiter, boolean shouldPrintHelp) {
@@ -23,69 +23,37 @@ public class ParserResult {
         this.shouldPrintHelp = shouldPrintHelp;
     }
 
-    //region Setters and Getters
+    //region Getters
     public String getFrom() {
         return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
     }
 
     public String getTo() {
         return to;
     }
 
-    public void setTo(String to) {
-        this.to = to;
-    }
-
     public String getFromOption() {
         return fromOption;
-    }
-
-    public void setFromOption(String fromOption) {
-        this.fromOption = fromOption;
     }
 
     public String getToOption() {
         return toOption;
     }
 
-    public void setToOption(String toOption) {
-        this.toOption = toOption;
-    }
-
     public boolean getShouldPrintHelp() {
         return shouldPrintHelp;
-    }
-
-    public void setShouldPrintHelp(boolean shouldPrintHelp) {
-        this.shouldPrintHelp = shouldPrintHelp;
     }
 
     public String getInputFile() {
         return inputFile;
     }
 
-    public void setInputFile(String inputFile) {
-        this.inputFile = inputFile;
-    }
-
     public String getOutputFile() {
         return outputFile;
     }
 
-    public void setOutputFile(String outputFile) {
-        this.outputFile = outputFile;
-    }
-
     public String getDelimiter() {
         return delimiter;
-    }
-
-    public void setDelimiter(String delimiter) {
-        this.delimiter = delimiter;
     }
     //endregion
 
