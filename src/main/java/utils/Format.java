95836476e259c2618d4a32829b41a6b9fcc31d66
Michal Badin <badin.miso@gmail.com>
2023-03-14 09:21:33 +0100

    Adjusted format enum to hold specific converter to each type
    Signed-off-by: Michal Badin <badin.miso@gmail.com>

--- src/main/java/utils/Format.java
+++ src/main/java/utils/Format.java
@@ -1,23 +1,34 @@
 package utils;
 
 
+import converters.BytesConverter;
+import converters.HexConverter;
+import converters.IConverter;
+import converters.IntConverter;
+
 import java.util.Arrays;
 
 public enum Format {
-	INT("int"),
-	HEX("hex"),
-	BITS("bits"),
-	BYTES("bytes"),
-	ARRAY("array");
+	INT("int", new IntConverter()),
+	HEX("hex", new HexConverter()),
+	BITS("bits", new IntConverter()),
+	BYTES("bytes", new BytesConverter()),
+	ARRAY("array", new BytesConverter());
 
 	private final String text;
+	private final IConverter converter;
 
-	Format(String text) {
+	Format(String text, IConverter converter) {
 		this.text = text;
+		this.converter = converter;
 	}
 
 	public String getText() {
 		return text;
+	}
+
+	public IConverter getConverter() {
+		return converter;
 	}
 
 	public static boolean contains(String value) {
