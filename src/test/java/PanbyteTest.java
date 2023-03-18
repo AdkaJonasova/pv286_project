da89810b3264cadd7c04cf29381ce10fb4b362b5
Dubkoslove <dominik.dubovskyy@gmail.com>
2023-03-18 17:11:01 +0100

    Add oposit of init test for panbyte functional test
    Signed-off-by: Dubkoslove <dominik.dubovskyy@gmail.com>

--- src/test/java/PanbyteTest.java
+++ src/test/java/PanbyteTest.java
@@ -9,10 +9,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PanbyteTest {
 
     @Test
-    public void testCorrectOutputWhenPassedCorrectSimpleInputAndArgsFromTo() {
+    public void testInput74657374FromHexToBytesExpectedTest() {
         String echo = "74657374";
         String[] args = {"-f", "hex", "-t", "bytes"};
         String expectedOutput = "test";
+        String actualOutput = getOutputOfProgramCall(echo, args);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testInputTestFromBytesToHexExpected74657374() {
+        String echo = "test";
+        String[] args = {"-f", "bytes", "-t", "hex"};
+        String expectedOutput = "74657374";
         String actualOutput = getOutputOfProgramCall(echo, args);
         assertEquals(expectedOutput, actualOutput);
     }
