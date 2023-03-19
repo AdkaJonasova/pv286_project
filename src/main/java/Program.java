7c95e4f2753b0df579335377e75d8b042b16bbeb
AdkaJonasova <xjonaso1@fi.muni.cz>
2023-03-19 15:55:19 +0100

    Corrected if print help do not ask for value
    Signed-off-by: AdkaJonasova <xjonaso1@fi.muni.cz>

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -11,17 +11,20 @@ public class Program {
         try {
             var result = inputParser.parse(args);
 
+            // if program should print help, don't ask for input
+            if (result.getShouldPrintHelp()) {
+                HelpPrinter.printHelp();
+                return;
+            }
+
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please enter value you want to convert: ");
             var valueToConvert = scanner.nextLine();
 
-            if (result.getShouldPrintHelp()) {
-                HelpPrinter.printHelp();
-            }
-
             var convertedFromVal = result.getFrom().getConverter().convertFrom(valueToConvert, result.getFromOption());
             var convertedValue = result.getTo().getConverter().convertTo(convertedFromVal, result.getToOption());
             System.out.println(convertedValue);
+
         } catch (InputParsingException e) {
             System.out.println("ERROR: " + e.getMessage());
             System.exit(1);
