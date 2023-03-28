15751ee7fb9460f172af91715e38f708fbb3db9e
Michal Badin <badin.miso@gmail.com>
2023-03-28 16:12:49 +0200

    feat(format): Added regex for ArrayConverter values
    Signed-off-by: Michal Badin <badin.miso@gmail.com>

--- src/main/java/format/Format.java
+++ src/main/java/format/Format.java
@@ -5,20 +5,23 @@ import converters.*;
 import java.util.Arrays;
 
 public enum Format {
-	INT("int", new IntConverter(), "int = Integer"),
-	HEX("hex", new HexConverter(), "hex = Hex-encoded string"),
-	BITS("bits", new BitsConverter(), "bits = 0,1-represented bits"),
-	BYTES("bytes", new BytesConverter(), "bytes = Raw bytes"),
-	ARRAY("array", new ArrayConverter(), "array = Byte array");
+	INT("int", new IntConverter(), "int = Integer", "^(\\d)$"),
+	HEX("hex", new HexConverter(), "hex = Hex-encoded string", "^0x([0-9a-fA-F]{2})$"),
+	BITS("bits", new BitsConverter(), "bits = 0,1-represented bits", "^0b([01]{1,8})$"),
+	BYTES("bytes", new BytesConverter(), "bytes = Raw bytes", "^'\\\\x([0-9a-fA-F]{2})'$"),
+	ARRAY("array", new ArrayConverter(), "array = Byte array", "");
 
 	private final String text;
 	private final Converter converter;
 	private final String description;
+	private final String arrayRegex;
 
-	Format(String text, Converter converter, String description) {
+
+	Format(String text, Converter converter, String description, String arrayRegex) {
 		this.text = text;
 		this.converter = converter;
 		this.description = description;
+		this.arrayRegex = arrayRegex;;
 	}
 
 	public String getText() {
@@ -33,9 +36,20 @@ public enum Format {
 		return description;
 	}
 
+	public String getArrayRegex() { return arrayRegex; }
+
 	public static Format fromString(String text) {
 		for (Format format : Format.values()) {
 			if (format.getText().equalsIgnoreCase(text)) {
+				return format;
+			}
+		}
+		return null;
+	}
+
+	public static Format getFormatFromReg(String input){
+		for (Format format : Format.values()) {
+			if (input.matches(format.arrayRegex)) {
 				return format;
 			}
 		}
