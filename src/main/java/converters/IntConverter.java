a91c4fd62363c7b91771f43090eca96f48da8ee0
Michal Badin <badin.miso@gmail.com>
2023-04-05 19:14:56 +0200

    refactoring(IntConverter): Extracted methods
    Signed-off-by: Michal Badin <badin.miso@gmail.com>

--- src/main/java/converters/IntConverter.java
+++ src/main/java/converters/IntConverter.java
@@ -4,6 +4,8 @@ import exceptions.ConverterException;
 import options.IOption;
 import options.IntOption;
 
+import java.util.Objects;
+
 import static options.IntOption.BIG;
 import static options.IntOption.LITTLE;
 
@@ -11,45 +13,49 @@ public class IntConverter extends Converter {
 	@Override
 	public String convertTo(String bitStr, IOption[] options) throws ConverterException {
 		validateInput(bitStr, "^[01 ]+$");
+		IntOption endian = getEndianFromOptions(options);
 
-		IntOption endian = options == null || options.length == 0 || options[0] == null ? BIG : (IntOption) options[0];
-
-		if (LITTLE.equals(endian)){
-			StringBuilder builder = new StringBuilder();
-			bitStr = addMissingZerosToBitString(bitStr);
-
-			for (int i = bitStr.length() - 1; i >= 0; i -= 8) {
-				int startIndex = Math.max(i - 7, 0);
-				String chunk = bitStr.substring(startIndex, i + 1);
-				builder.append(chunk);
-			}
-			bitStr = builder.toString();
-		}
-
+		bitStr = processBitString(bitStr, endian);
 		long unsignedInt = Long.parseUnsignedLong(bitStr, 2);
+
 		return Long.toUnsignedString(unsignedInt);
 	}
 
 	@Override
 	public String convertFrom(String input, IOption[] options) throws ConverterException {
 		validateInput(input, "^\\d+$");
-
-		IntOption endian = options == null || options.length == 0 || options[0] == null ? BIG : (IntOption) options[0];
+		IntOption endian = getEndianFromOptions(options);
 
 		long unsignedInt = Long.parseUnsignedLong(input);
 		String bitStr = Long.toBinaryString(unsignedInt);
-		StringBuilder result = new StringBuilder();
+		bitStr = processBitString(bitStr, endian);
 
+		return bitStr;
+	}
+
+	private String processBitString(String bitStr, IntOption endian) {
 		bitStr = addMissingZerosToBitString(bitStr);
-
-		if (LITTLE.equals(endian)){
-			for (int i = bitStr.length() - 1; i >= 0; i -= 8) {
-				int startIndex = Math.max(i - 7, 0);
-				String chunk = bitStr.substring(startIndex, i + 1);
-				result.append(chunk);
-			}
-			return result.toString();
+		if (LITTLE.equals(endian)) {
+			bitStr = changeToLittleEndian(bitStr);
 		}
 		return bitStr;
+	}
+
+	private String changeToLittleEndian(String bitStr) {
+		StringBuilder builder = new StringBuilder();
+		for (int i = bitStr.length() - 1; i >= 0; i -= 8) {
+			int startIndex = Math.max(i - 7, 0);
+			String byteStr = bitStr.substring(startIndex, i + 1);
+			builder.append(byteStr);
+		}
+		return builder.toString();
+	}
+
+	private IntOption getEndianFromOptions(IOption[] options) throws ConverterException {
+		try {
+			return Objects.isNull(options) || options.length == 0 || options[0] == null ? BIG : (IntOption) options[0];
+		} catch (Exception e) {
+			throw new ConverterException(String.format("IntConverter doesn't support option: %s", options[0]));
+		}
 	}
 }
